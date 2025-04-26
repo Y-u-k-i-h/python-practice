32233682721886b10bf4c7ef8792de0059487752
Yukih <4rnold.richard@gmail.com>
2025-04-26 16:24:51 +0300

    Fixed some typos

--- Exercises/sum_of_digits.py
+++ Exercises/sum_of_digits.py
@@ -1,17 +1,17 @@
 def sum_of_digits(n):
-    # i did this because if it's a single digit, then the sum would have to be the number itself
-    # so i needed to return the range which contains single digits
-    # initialized a variable to hold the sum too
+    # initialized a variable to hold the sum of the numbers
     total_sum = 0
+    # I did this because if it's a single digit, then the sum would have to be the number itself
+    # so I needed to return the range which contains single digits
     if n  >= -9 and n <= 9:
-        return n
+        return abs(n)
     else:
-        # i decided to also convert the number to a string
-        # this is to also help me iterate through individual digits
+        # converted the number to a string
+        # to help me iterate through individual digits
         n = str(n)
         for digit in n:
-            # here i stripped off the '-' since it will throw an error as one can't add
-            # a character and a number
+            # here I stripped off the '-' since it will throw an error as `-` can't be converted
+            # to type int
             if digit == '-':
                 continue
             total_sum += int(digit)
